f95e3f8d4f7265c8f944a968fa4189371d21ece7
0x0Tamim <mmtamimuzzaman@gmail.com>
2026-05-14 21:39:49 +0600

    Updated with list,tuple,set practise

--- Day1.py
+++ Day1.py
@@ -52,3 +52,41 @@ print(f"My name is {name} and my cgpa is {cgpa}")
 namee = "tamim uz zaman"
 newName = namee.title()
 print(newName)
+
+list = [1,2,3,"Tamim",4,5]
+print(list)
+print(list[-1])
+print(list[2:4])
+print(list[::-1])
+list.append(6)
+print(list)
+list.insert(3,"Tisha")
+print(list)
+print(list.count(2))
+print(3 in list)
+print(len(list))
+
+print('Tisha' in list)
+print(list.index('Tisha'))
+
+for x in list:
+    print(x)
+
+for i in range(len(list)):
+    print(list[i])
+
+even = [x for x in range(10) if x%2==0]
+print(even)
+
+tuple = (2,3,4,5,"tamim",6,"tisha")
+print(tuple)
+
+set = {1,4,4,2,"tamim","tisha","tamim",5,6}
+print(set)
+
+a = {1,2,3,4}
+b = {3,4,5,6}
+print(a|b)
+print(a&b)
+print(a-b)
+print(a^b)
